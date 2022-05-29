e42264c05811a334c88e20e4b3ba46a253efae35
Victor Grosu <vagrosu@yahoo.com>
2022-05-30 01:47:45 +0300

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from 'react-router-dom';
+
+import {
+  HomePage,
+  PokedexPage,
+  PokeInfoPage,
+  LegendariesPage,
+  AboutPage,
+  NotFoundPage,
+} from './pages/index';
 
 function App() {
-  return <div>pokedex</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/pokedex" element={<PokedexPage />} />
+        <Route path="/pokemonInfo:id" element={<PokeInfoPage />} />
+        <Route path="/legendaries" element={<LegendariesPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="*" exact element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as AboutPage } from './About/About';
+export { default as HomePage } from './Home/Home';
+export { default as LegendariesPage } from './Legendaries/Legendaries';
+export { default as NotFoundPage } from './NotFound/NotFound';
+export { default as PokedexPage } from './Pokedex/Pokedex';
+export { default as PokeInfoPage } from './PokeInfo/PokeInfo';
