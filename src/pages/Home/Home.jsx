af58372406d8298eaa3cde282e09bb1fd86c6403
Victor Grosu <vagrosu@yahoo.com>
2022-05-31 01:55:44 +0300

    Update HomeScreen

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import Button from '../../components/button/Button';
+// import pikachuCut from '../../assets/pikachuCut.svg';
+import pikachuFull from '../../assets/pikachuFull.svg';
 
 import styles from './Home.module.css';
 
 function Home() {
+  const navigate = useNavigate();
+
+  const toPokedexHandler = () => {
+    navigate('/pokedex', { state: true });
+  };
+
   return (
-    <div>Home</div>
+    <div className={styles.home}>
+      <div className={styles.container}>
+        <div className={styles.textContainer}>
+          <h1 className={styles.title}>
+            Find all your favorite Pokémon
+          </h1>
+          <h3 className={styles.subtitle}>
+            You can know the type of Pokémon, its strengths, disadvantages and abilities
+          </h3>
+          <Button type="primary" onClick={toPokedexHandler} className={styles.button}>See Pokémons</Button>
+        </div>
+        <div className={styles.imageContainer}>
+          <img src={pikachuFull} alt="Pikachu" />
+        </div>
+      </div>
+    </div>
   );
 }
 
